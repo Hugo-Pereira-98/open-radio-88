8a6cec7806697626dd534ed10158ed9dc3c0db66
Hugo Pereira <hugop868@gmail.com>
2024-07-31 11:38:40 -0300

    chore: Update _app.tsx to adjust background color and text size

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -216,9 +216,9 @@ export default function App({ Component, pageProps }: AppProps) {
               </div>
             </div>
           </div>
-          <div className="w-full bg-[#f9f9f9] pb-6">
-            <div className="relative w-full sm:w-2/3 h-auto bg-[#f9f9f9] mx-auto p-6 pb-0">
-              <p className="text-center text-lg sm:text-2xl">
+          <div className="w-full bg-[#f9f9f9]">
+            <div className="relative w-full sm:w-2/3 h-auto bg-[#f9f9f9] mx-auto p-6 py-20">
+              <p className="text-center text-lg sm:text-2xl mb-6">
                 "<strong>DAMOS GRAÇAS A DEUS</strong> PELO QUE PASSOU{' '}
                 {isSmallerThan850 ? ' ' : <br />}
                 PELO DIA DE HOJE E PELO DIA QUE VIRÁ."
